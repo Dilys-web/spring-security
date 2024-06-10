5777f0398890d294d3b13c28053fe3cf9c7762e3
Dilys-web <opokudonkord@gmail.com>
2024-06-10 11:22:47 +0000

    feat: Exposes AuthenticationController for JWT login (/authenticate) and registration (/register) endpoints

---
+++ src/main/java/com/jwt/spring_security/auth/AuthenticationController.java
@@ -0,0 +1,29 @@
+package com.jwt.spring_security.auth;
+
+
+import com.jwt.spring_security.dto.request.AuthenticationRequest;
+import com.jwt.spring_security.dto.request.RegisterRequest;
+import com.jwt.spring_security.dto.response.AuthenticationResponse;
+import com.jwt.spring_security.services.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+}
